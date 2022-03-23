87c1f1c24dea7891a8d0ae8f30fa73a1b49683a0
Md Asaduzaman <asadasad9282@gmail.com>
2022-03-23 18:10:14 +0600

    set local storage and get local storage

--- src/compoonent/Shop/shop.js
+++ src/compoonent/Shop/shop.js
@@ -1,33 +1,55 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredcart } from '../../utilities/fakedb'
 import Cart from '../cart/Cart';
 import Product from '../product/Product';
 import './Shop.css'
 
 const Shop = () => {
-
     const [products, setProducts] = useState([])
-
     const [cart, setCart] = useState([])
 
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
+
     }, [])
+    useEffect(() => {
+        const storedCart = getStoredcart()
+        const savecart = []
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id)
+            if (addedProduct) {
+                const quantity = storedCart[id]
+                addedProduct.quantity = quantity
+                savecart.push(addedProduct)
+            }
+        }
+        setCart(savecart)
 
-    const handelAddToCart = (product) => {
-        console.log(product);
-        const newCart = [...cart, product]
+    }, [products])
+
+
+    const handelAddToCart = (selectedProduct) => {
+        let newCart = []
+        const exists = cart.find(product => product.id === selectedProduct.id)
+        if (!exists) {
+            selectedProduct.quantity = 1
+            newCart = [...cart, selectedProduct]
+        } else {
+            const rest = cart.filter(product => product.id !== selectedProduct.id)
+            exists.quantity = exists.quantity + 1
+            newCart = [...rest, exists]
+        }
+
         setCart(newCart)
+        addToDb(selectedProduct.id)
     }
-
-
 
 
     return (
         <div>
             <div className="shop-container">
-
                 <div className="product-container">
                     {
                         products.map(product => <Product
@@ -42,7 +64,6 @@ const Shop = () => {
                 <div className="cart-container">
                     <Cart cart={cart}></Cart>
                 </div>
-
             </div>
         </div>
     );
--- src/compoonent/cart/Cart.js
+++ src/compoonent/cart/Cart.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import './Cart.css'
 const Cart = ({ cart }) => {
+    console.log(cart);
 
-    let price = 0;
+    let total = 0;
     let shiping = 0;
+    let quantity = 0;
 
     for (const product of cart) {
-        price = price + product.price
+        quantity = quantity + product.quantity
+        total = total + product.price * product.quantity
         shiping = shiping + product.shipping
     }
-    let txt = (price * 0.1).toFixed(2)
-    let grandtotal = price + shiping + parseFloat(txt)
+    let txt = (total * 0.1).toFixed(2)
+    let grandtotal = total + shiping + parseFloat(txt)
 
     return (
         <div className='cart'>
 
             <h4>Order Summery</h4>
-            <p>Selected item : {cart.length}</p>
-            <p>Total Price: ${price}</p>
+            <p>Selected item : {quantity}</p>
+            <p>Total Price: ${total}</p>
             <p>Total Shiping: ${shiping} </p>
             <p>tax: ${txt} </p>
             <h4>grand Total: $ {grandtotal.toFixed(2)}</h4>
