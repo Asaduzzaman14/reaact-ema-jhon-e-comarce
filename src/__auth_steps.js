468d89afa56026a08fb603c32d735f499ac09d13
Md Asaduzaman <asadasad9282@gmail.com>
2022-04-10 17:07:00 +0600

    can not create user

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -1,11 +1,16 @@
 /**  
  * 
- * 1. create a new project in console.google.com 
+ * 1. create a new firebase project in console.firebase.google.com 
  * 2. npm install fairbase
  * 3. create fairbase.init.js and import get auth to getauth
  * 4. firebase setting > Authentication > eneble email and password auth
  * 5. Create Login, singUp compponent, setU route
- * attach form fild handler and form submit handler
+ * 6.attach form fild handler and form submit handler
+ * 7.npm install --save react-firebase-hooks
+ * 
+ * 
+ * 
+
 
 
 */
--- src/compoonent/SignUp/SignUp.js
+++ src/compoonent/SignUp/SignUp.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init'
+
+
 
 const SignUp = () => {
 
@@ -8,22 +12,51 @@ const SignUp = () => {
     const [confirmPassword, setConfirmPassword] = useState('')
     const [error, setError] = useState('')
 
+    const navigate = useNavigate()
+
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth)
+
     const handelEmailBlur = event => {
         setEmail(event.target.value)
+        console.log(email);
     }
+
     const handelPasswordBlur = event => {
         setPassword(event.target.value)
+        console.log(password);
     }
+
     const handelconfirmPasswordBlur = event => {
         setConfirmPassword(event.target.value)
+        console.log(confirmPassword);
+
+    }
+
+    if (user) {
+        navigate('/inventory')
     }
 
     const handelCreatUser = event => {
         event.preventDefault()
-        if (password !== confirmPassword)
+
+
+        if (password !== confirmPassword) {
             setError('your confirm password did not match')
-        console.log(error);
-        return
+            return
+        }
+        if (password.length < 6) {
+            setError('password must be 6 character or longer')
+            return
+        }
+
+        createUserWithEmailAndPassword(email, password)
+            .then((result) => {
+                // const user = result.user
+                console.log(result);
+                console.log("this is result");
+            })
+
+        // createUserWithEmailAndPassword(email, password)
 
     }
 
@@ -34,7 +67,9 @@ const SignUp = () => {
             <div className='form-contianer'>
                 <div>
                     <h1 className='form-title'>SignUp</h1>
+
                     <form onSubmit={handelCreatUser}>
+
                         <div className='input-group'>
                             <label htmlFor="email">Email</label>
                             <input onBlur={handelEmailBlur} type="email" name='email' required />
@@ -50,7 +85,7 @@ const SignUp = () => {
                             <input onBlur={handelconfirmPasswordBlur} type="password" name='password' required />
                         </div>
                         <p style={{ color: 'red' }}>
-                            {error ? error : ''}
+                            {error.message}
 
                         </p>
                         <input className='form-submit' type="Sign Up" value="Submit" />
